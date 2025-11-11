5284133623ec0c6c9ff4575df0319386009d3fba
lastdaykw0n <lastdaykw0n@gmail.com>
2025-11-11 21:45:39 +0900

    chore: [상단배너] 배너 mock 데이터 추가

---
+++ src/mocks/bannerData.ts
@@ -0,0 +1,41 @@
+import type { Banner } from '@/features/banner/entities/Banner';
+
+export const mockBanners: Banner[] = [
+  {
+    id: 'mapo-airdrop',
+    image: {
+      kr: '/banner/banner_mapo_kr.png',
+      en: '/banner/banner_mapo_en.png',
+    },
+    link: {
+      kr: 'https://store-kr.dcentwallet.com/blogs/post/tap-that-drop-with-map-protocol',
+      en: 'https://store.dcentwallet.com/blogs/post/tap-that-drop-with-map-protocol',
+    },
+  },
+  {
+    id: 'dcent-wallet',
+    image: { kr: '/banner/banner_dcent.png', en: '/banner/banner_dcent.png' },
+    description: {
+      kr: '디센트 지문인증형 지갑으로 한층 더 강화된 보안을 경험하세요!',
+      en: "Enhance your security with D'CENT biometric wallet",
+    },
+    link: {
+      kr: 'https://store-kr.dcentwallet.com',
+      en: 'https://store.dcentwallet.com',
+    },
+    buttonText: { kr: '구매하기', en: 'Buy Now' },
+  },
+  {
+    id: 'dcent-blog',
+    image: { kr: '/banner/banner_blog.png', en: '/banner/banner_blog.png' },
+    description: {
+      kr: '새로운 디센트 블로그를 방문하여 최신 업데이트를 먼저 확인해보세요!',
+      en: 'Visit the new D’CENT Blog to explore the latest updates first!',
+    },
+    link: {
+      kr: 'https://store-kr.dcentwallet.com/blogs/post',
+      en: 'https://store.dcentwallet.com/blogs/post',
+    },
+    buttonText: { kr: '확인하기', en: 'Explore' },
+  },
+];
